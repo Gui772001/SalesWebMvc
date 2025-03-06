f57738966ffd93cb600f6556230e6aee36aa2c4a
Guilherme Augusto <gulhermegui772001@gmail.com>
2025-03-06 13:26:17 -0300

    edit e delete funcionando

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -38,6 +39,83 @@ namespace SalesWebMvc.Controllers
             _selleService.Insert(seller);
 
             return RedirectToAction(nameof(Index));
+
+        }
+        public IActionResult Delete(int? id)
+        {
+           if( id == null)
+            {
+                return NotFound();
+            }
+            var obj = _selleService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _selleService.Remove(id);
+            return RedirectToAction(nameof(Index));
+
+        }
+           
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _selleService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _selleService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj,Departments = departments };
+            return View(viewModel);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,Seller seller)
+        {
+          if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _selleService.Uptade(seller);
+                return RedirectToAction(nameof(Index));
+            }catch(NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- SalesWebMvc/Services/SelleService.cs
+++ SalesWebMvc/Services/SelleService.cs
@@ -1,4 +1,6 @@
 ﻿using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -22,5 +24,32 @@ namespace SalesWebMvc.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+        public void Uptade(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id) )
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+
+                _context.Update(obj);
+                _context.SaveChanges();
+            }catch(DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
     }
 }
